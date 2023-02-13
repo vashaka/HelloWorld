cd26c714873a7910209f770088a8410fa3fe0e79
irakli imesashvili <i.imesashvili@betsolutions.com>
2023-02-13 11:59:35 +0400

    Add project files.

---
+++ HelloWorld/SimpleHelloWorld.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    public static class SimpleHelloWorld
+    {
+        public static string ReturnHelloWorld()
+        {
+            const string statement = "Hello, world!";
+            return statement;
+        }
+    }
+}
