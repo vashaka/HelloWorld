1e55d37565e14a0d0adcf14f1f3eab4dfe710c30
vashaka <83545222+vashaka@users.noreply.github.com>
2024-12-17 23:42:45 -0800

    Task 1 Done

--- HelloWorld/SimpleHelloWorld.cs
+++ HelloWorld/SimpleHelloWorld.cs
@@ -1,13 +1,10 @@
 ﻿namespace HelloWorld
 {
-    
     public static class SimpleHelloWorld
     {
-        
         public static string ReturnHelloWorld()
         {
             // TODO: Return string "Hello, world!";
-            
             var greeting = "Hello, world!";
             return greeting;
         }
